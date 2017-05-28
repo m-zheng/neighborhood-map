9b2de9e197683f20a226ee777d972a93270d358f
Minghua <m.zheng1991@outlook.com>
2017-05-28 09:11:38 +0000

    update comment and readme

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # About
 
 This project is a part of Full Stack Developer Nanodegree from [Udacity](https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004).
-It provides a single-page application featuring a map of london attractions. Users can click markers to get the infomation of the place from wikipedia. Users can  also search these locatins in a search bar, all results will pop up without reloading the map.
+It provides a single-page application featuring a map of london attractions. Users can click markers to get the infomation of the place from wikipedia. Users can also search these locatins in a search bar, all results will pop up without reloading the page.
 
 ## Getting Started
   - Download the repository and open index.html in a web browser. 
+
+## Specification
+  - Google Maps Api
+  - Wiki Api
+  - jQuery
+  - Knockout JS
+
+## Contributors
+  - Thank [Tanveer](https://github.com/tan-ahmed) for beautifying the front end.
+
+  
 
 
 License
--- js/app.js
+++ js/app.js
@@ -99,17 +99,16 @@ function googleError() {
 }
 
 /**
- * @description load google map via google map api
+ * @description Load google map via google map api.
+ * Constructor creates a new map, only center and zoom are required.
  */
 function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 51.500729,
             lng: -0.124603
         },
         zoom: 13,
-        // STYLES START
         styles: [{
                 "featureType": "landscape.man_made",
                 "elementType": "geometry",
@@ -216,17 +215,22 @@ function initMap() {
                 }]
             }
         ]
-
-        // STYLES END
     });
 
-    // This function takes in a COLOR, and then creates a new marker
-    // icon of that color. The icon will be 21 px wide by 34 high,
-    // have an origin of 0, 0 and be anchored at 10, 34).
+    /**
+     * @description It creates a new marker icon of that color.
+     * have an origin of 0, 0 and be anchored at 10, 34).
+     * It uses the location array to create an array of markers on initialize.
+     * And add event listener to marker on click, mouseover and mouseout.
+     * It extends the boundaries of the map for each marker,
+     * and make the map responsive.
+     * @param {string} markerColor - COLOR 
+     */
     function makeMarkerIcon(markerColor) {
         var markerImage = new google.maps.MarkerImage(
-            'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-            '|40|_|%E2%80%A2',
+            'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'
+            + markerColor
+            + '|40|_|%E2%80%A2',
             new google.maps.Size(21, 34),
             new google.maps.Point(0, 0),
             new google.maps.Point(10, 34),
@@ -234,24 +238,14 @@ function initMap() {
         return markerImage;
     }
 
-    // Style the markers a bit. This will be listing marker icon.
     var defaultIcon = makeMarkerIcon('f75850');
-
-    // Create a 'highlighted location' marker color for when the user
-    // mouses over the marker.
     var highlightedIcon = makeMarkerIcon('FFFF24');
-
     var largeInfowindow = new google.maps.InfoWindow();
-
     var bounds = new google.maps.LatLngBounds();
-
-    // Create a new blank array for all the listing markers.
     var markers = [];
 
-    // The following group uses the location array to create an array of markers on initialize.
     for (var i = 0; i < locations.length; i++) {
         var title = locations[i].title;
-        // Create a marker per location, and put into markers array.
         var marker = new google.maps.Marker({
             map: map,
             position: {
@@ -263,37 +257,25 @@ function initMap() {
             icon: defaultIcon,
             id: i
         });
-        // Push the marker to our array of markers.
         markers.push(marker);
-
         viewModel.locationList()[i].marker = marker;
-
-        // Create an onclick event to open an infowindow at each marker.
-        // Animate the click event
         marker.addListener('click', (function(marker) {
             return function() {
                 marker.setAnimation(google.maps.Animation.BOUNCE);
                 setTimeout(function() {
-                    marker.setAnimation(null)
+                    marker.setAnimation(null);
                 }, 750);
                 populateInfoWindow(marker, largeInfowindow);
             };
         })(marker));
-
-        // Two event listeners - one for mouseover, one for mouseout,
-        // to change the colors back and forth.
         marker.addListener('mouseover', function() {
             this.setIcon(highlightedIcon);
         });
         marker.addListener('mouseout', function() {
             this.setIcon(defaultIcon);
         });
-
         bounds.extend(markers[i].position);
     }
-
-    // Extend the boundaries of the map for each marker
-    // And make the map responsive
     google.maps.event.addDomListener(window, 'resize', function() {
         map.fitBounds(bounds);
     });
@@ -302,40 +284,56 @@ function initMap() {
 
 
 /**
- * @description This function populates the infowindow when the marker is clicked.
+ * @description This function populates the infowindow
+ * when corresponding marker is clicked.
  * Only one infowindow which will open at the marker that is clicked,
  * and populate based on that markers position.
+ * The default content for infowindow is mark title plus error message.
+ * But when the marker is clicked, ajax request will be fired to update content.
+ * A closeclick event listener is add to infowindow as well.
  */
 function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
-        // Update marker title and wiki summary to infowindow
-        var wikiURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + marker.title
+        var content = '';
+        content += '<h1 class="marker-title">'
+                + marker.title
+                + '</h1><p class="marker-summary">Oops! Something went wrong</p>';
+        
+        var wikiURL = 'https://en.wikipedia.org/w/api.php'
+                    + '?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='
+                    + marker.title;
+       
         $.ajax({
             url: wikiURL,
             method: "GET",
             dataType: "jsonp",
         }).done(function(data) {
             $.each(data.query.pages, function(i, item) {
-                var content = '';
+                content = '';
                 var wikiPage = "https://en.wikipedia.org/?curid=" + i;
                 var wikiSummary = item.extract.substring(0, 250);
-                content += '<h1 class="marker-title">' + marker.title + '</h1><p class="marker-summary">' + wikiSummary + ' ...<a target="_blank" href="' + wikiPage + '">Read More</a> </p>';
+                content += '<h1 class="marker-title">'
+                        + marker.title
+                        + '</h1><p class="marker-summary">'
+                        + wikiSummary
+                        + ' ...<a target="_blank" href="'
+                        + wikiPage
+                        + '">Wikipedia</a> </p>';
                 infowindow.setContent(content);
             });
 
         }).fail(function() {
-            var content = '';
-            content += ' <h3> ' + marker.title + ' </h3> <p>Oops! Something went wrong</p>';
+            content = '';
+            content += '<h1 class="marker-title">'
+                    + marker.title
+                    + '</h1><p class="marker-summary">Oops! Something went wrong</p>';
             infowindow.setContent(content);
         });
 
         infowindow.marker = marker;
-        // Make sure the marker property is cleared if the infowindow is closed.
         infowindow.addListener('closeclick', function() {
             infowindow.marker = null;
         });
-        // Open the infowindow on the correct marker.
         infowindow.open(map, marker);
     }
 }
@@ -350,7 +348,9 @@ var Location = function(data) {
 };
 
 /**
- * @description ViewModel
+ * @description It will filter user's input and return matched results.
+ * When the menu button is clicked,
+ * it will add toggle class to the element to show the search bar.
  */
 var ViewModel = function() {
     var self = this;
@@ -378,7 +378,8 @@ var ViewModel = function() {
                 singleLocation.marker.setVisible(true);
             }
 
-            if (singleLocation.title.toLowerCase().indexOf(self.query().toLowerCase()) !== -1 && singleLocation.marker) {
+            if (singleLocation.title.toLowerCase().indexOf(self.query().toLowerCase()) !== -1
+                && singleLocation.marker) {
                 singleLocation.marker.setVisible(true);
             }
 
@@ -386,10 +387,8 @@ var ViewModel = function() {
         });
     });
 
-    // The default value for toggledNav is false
     this.toggledNav = ko.observable(false);
-
-    // When the menu button is clicked, it will add toggle class to the element.
+    
     this.triggerToggle = function() {
         this.toggledNav(!this.toggledNav());
     };
